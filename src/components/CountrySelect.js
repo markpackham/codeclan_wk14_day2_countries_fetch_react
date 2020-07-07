20041e583189e44b12479dff182ea3fe7d15debd
mark <markpackham1@gmail.com>
2020-07-07 11:54:13 +0100

    add selector

--- src/components/CountrySelect.js
+++ src/components/CountrySelect.js
@@ -5,8 +5,12 @@ const optionsList = props.countries.map(country =>{
 return <option value={country.alpha3Code} key={country.alpha3Code}>{country.name}</option>
 });
 
+function handleChange(event){
+    console.log(event.target.value);
+}
+
   return (
-    <select id="country-selector" defaultValue="default">
+    <select id="country-selector" defaultValue="default" onChange={handleChange}>
       <option disabled value="default">Choose a country...</option>
       {optionsList}
     </select>
--- src/containers/CountryContainer.js
+++ src/containers/CountryContainer.js
@@ -8,15 +8,19 @@ class CountryContainer extends React.Component {
         countries: [],
         selectedCountryAlpha3Code: ''
     };
+
+    this.handleCountrySelected = this.handleCountrySelected.bind(this);
   }
 
 componentDidMount(){
     const url = 'https://restcountries.eu/rest/v2/all';
-
     fetch(url)
     .then(res => res.json())
     .then(data => this.setState({countries: data}))
+}
 
+handleCountrySelected(alpha3Code){
+    this.setState({selectedCountryAlpha3Code: alpha3Code});
 }
 
   render(){
@@ -25,6 +29,7 @@ componentDidMount(){
         <h2>Country Container</h2>
         <CountrySelect 
         countries={this.state.countries}
+        onCountrySelected = {this.handleCountrySelected}
         />
         <CountryDetail />
       </div>
